f57270d8346591fa5e6c653e250df685e1dc9f13
Vane Arochi <vanearochi@gmail.com>
2015-10-30 16:15:05 -0700

    all the operations can be done, AC and CE work too. Operation can be done after having the result using it.Displaying is working too

--- media/js/calculator1.js
+++ media/js/calculator1.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var regexNum=/\d/;
 	var temporalStorage="";
 	var result;
+	var backupArr=[];
 console.log("Welcome calculator")
 //arr to push 
 var pushvalue = function(val){
@@ -15,51 +16,76 @@ var pushvalue = function(val){
 	else if(val==="ac"){
 		arrPushedValue=[];
 		temporalStorage="";
+		backupArr=[];
+		display(0)
 	}
 	else if(val==="ce"){
 		if(temporalStorage.length>0){
 		  	temporalStorage=""
+		  	display(arrPushedValue)
 		}
 		else if(arrPushedValue.length>0){
 			arrPushedValue.pop()
+			display(arrPushedValue)
 		}
 	}
-	else{
-
-		if(arrPushedValue[arrPushedValue.length-1]==="*"){
-           arrPushedValue.pop()
-           temporal =arrPushedValue.pop()*temporalStorage;
-           arrPushedValue.push(temporal);
-           arrPushedValue.push(val);
-           temporalStorage="";
-		}
+	
 		else if(temporalStorage.length>0){
 		temporalStorage=parseFloat(temporalStorage);
 		arrPushedValue.push(temporalStorage);
 		arrPushedValue.push(val)
 		temporalStorage=""
+		console.log("test11"+arrPushedValue)
 		}
 		else if(arrPushedValue.length>0){
 			if(regexNum.test(arrPushedValue[arrPushedValue.length-1])===true){
 				
 				arrPushedValue.push(val);
+				console.log("test12"+arrPushedValue)
 			}
 			else{
 			arrPushedValue.pop();
 			arrPushedValue.push(val);
+			console.log("test13"+arrPushedValue)
 			}
 		}
-	}
+	
 	
 }
 
-var processResult = function(arr){
-	if(temporalStorage.length>0){
-		pushvalue("=")
+var multiplication = function(arr){
 		
-	}
+		var arrTemporal = [];
+		var multiplicationProduct;
+		for (var i = 0; i < arr.length-1; i++){
+			if(arr[i]==="*" && arr[i-2]==="*"){
+				multiplicationProduct=arrTemporal.pop()*arr[i+1];
+				arrTemporal.push(multiplicationProduct);
+				i++
+			}
+			else if(arr[i]==="*"){
+				multiplicationProduct=arr[i-1]*arr[i+1];
+				arrTemporal.pop();
+				arrTemporal.push(multiplicationProduct);
+				i++
+			}
+			else{
+				arrTemporal.push(arr[i]);
+			}
+		}
+		arrPushedValue=arrTemporal
+		console.log( "test3 " + arrPushedValue)
+
+};
+
+
+var processResult = function(arr){
+	
+	
+	
 	for (var i = 0; i < arr.length-1; i++) {
 		if(regexNum.test(arr[i])===false){
+			
     	 	if(i===1){
     	 		if(arr[i]==="+"){
     	 		   result=arr[i-1]+arr[i+1];
@@ -68,8 +94,9 @@ var processResult = function(arr){
     	 		  result=arr[i-1]-arr[i+1];
     	 		}
     	 		else if(arr[i]==="/"){
-    	 			result=arr[i-1]+arr[i+1];
+    	 			result=arr[i-1]/arr[i+1];
     	 		}
+    	 		//console.log("test1"+result)
     	 	}
     	 	else{
     	 		if(arr[i]==="+"){
@@ -81,11 +108,14 @@ var processResult = function(arr){
     	 		else if(arr[i]==="/"){
     	 			result=result/arr[i+1];
     	 		}
+    	 		console.log("test2"+result)
+    	 		console.log(typeof(result))
     	 	}
 		}
 	};
 	
-	
+	console.log(arrPushedValue)
+	console.log(result)
 }
 
 var display = function(val){
@@ -121,6 +151,17 @@ $(".one").on("click", function(){
  $(".ac").on("click", function(){pushvalue("ac")});
  $(".ce").on("click", function(){pushvalue("ce")});
  $(".equal").on("click", function(){
+
+ 	pushvalue("=")
+
+ 	if(arrPushedValue.indexOf("*")>=0){
+ 		multiplication(arrPushedValue)
+ 		if(arrPushedValue.length===1){
+ 			result=arrPushedValue[0]
+ 			display(result)
+ 		}
+ 		
+ 	}
  	
  	processResult(arrPushedValue)
  	arrPushedValue=[];
@@ -132,3 +173,13 @@ $(".one").on("click", function(){
 
 
 });//end ready
+
+
+// if(temporalStorage.length>0){
+//  		temporalStorage=parseFloat(temporalStorage);
+// 		arrPushedValue.push(temporalStorage);
+// 		console.log("test2 "+arrPushedValue )
+// 		temporalStorage=""
+		
+// 	}
+//  	
